0211cfd006ceb05461a2973951408fb3d9f7951d
DionneMaguire <dionnemaguire@yahoo.com>
2022-02-17 14:38:27 +0000

    add event listeners for DOM loading and button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,50 @@
+//* wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    console.log(buttons);
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                alert("there is a problem!!");
+            }
+        })
+    }
+})
+
 function runGame() {
-    
-}
-
-checkAnswer() {
 
 }
 
-calculateCorrectAnswer() {
+function checkAnswer() {
 
 }
 
-incrementScore() {
+function calculateCorrectAnswer() {
 
 }
 
-incrementWrongAnswer() {
+function incrementScore() {
 
 }
 
-displayAdditionQuestion() {
+function incrementWrongAnswer() {
 
 }
 
-displaySubtractQuestion() {
+function displayAdditionQuestion() {
 
 }
 
-displayMultiplyQuestion() {
+function displaySubtractQuestion() {
 
 }
 
-displayDivisionQuestion() {
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
 
 }
