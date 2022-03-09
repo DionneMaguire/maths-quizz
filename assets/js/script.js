763c73662d457035eb1697872b3d1b067ee16d32
DionneMaguire <dionnemaguire@yahoo.com>
2022-03-09 12:48:19 +0000

    remove unecessary class names

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // when user hits the submit answer button it will check answer
     let answer = document.getElementById("answer-button");
-    answer.addEventListener("onsubmit", function () {
+    answer.addEventListener("click", function () {
         checkAnswer();
     });
 
